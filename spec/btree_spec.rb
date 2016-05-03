9f431cb2f3def39c88f99c8e59bb7add1c5884be
asv0r <cdscott9@gmail.com>
2016-05-03 07:37:11 -0400

    Change test to match structure of btree.rb

--- spec/btree_spec.rb
+++ spec/btree_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe BinaryTree, type: :model do
     it 'should order the array' do
       expected = [1, 4, 6, 7, 9, 10, 14]
       array = [7, 4, 9, 1, 6, 14, 10]
-      expect(BinaryTree.sort(array)).to eq expected
+      tree = BinaryTree.new(7)
+      expect(tree.sort(array)).to eq expected
     end
   end
 end
