36c60bed729356d05e9044c1184665421a1a3949
asv0r <cdscott9@gmail.com>
2016-05-03 07:17:57 -0400

    Organize BinaryTree class, create skeleton method for binary_tree_sort

--- lib/btree.rb
+++ lib/btree.rb
@@ -1,37 +1,49 @@
 class BinaryTree
   attr_accessor :payload, :left, :right
 
-  def initialize(payload, left, right)
+  def initialize(payload, left = nil , right = nil)
     @payload = payload
     @left = left
     @right = right
   end
+
+  def build!(nums)
+    nums[1..-1].each do |num|
+      self.insert(num)
+    end
+  end
+
+  protected
 
   def insert(num)
     if num < payload
       if @left
         @left.insert(num)
       else
-        @left = BinaryTree.new(num, nil, nil)
+        @left = BinaryTree.new(num)
       end
     else
       if @right
         @right.insert(num)
       else 
-        @right = BinaryTree.new(num, nil, nil)
+        @right = BinaryTree.new(num)
       end
     end      
   end
 
+  def to_array
+
+  end
 end
 
-def build_tree(nums)
-  tree = BinaryTree.new(nums[0], nil, nil)
 
-  nums[1..-1].each do |num|
-    tree.insert(num)
-  end
 
+def binary_tree_sort(array) # returns array of sorted numbers
+  tree = BinaryTree.new(array[0])
+  tree.build!(array)
+  # tree.to_array
   tree
 end
 
+
+puts binary_tree_sort([7,4,9,1,6,14,10]).inspect
